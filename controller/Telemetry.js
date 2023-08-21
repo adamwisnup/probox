c1783bd8ef3c049381d47bd3b6a5f00e8e6726f5
adamwisnup <wisnuadam21@gmail.com>
2023-08-21 13:21:37 +0700

    change database with supabase

--- controller/Telemetry.js
+++ controller/Telemetry.js
@@ -6,14 +6,14 @@ const getTelemetryController = async (req, res) => {
     const telemetryDB = await telemetry.getLatestTelemetryDB();
 
     const getData = {
-      uid: telemetryDB[0].uid,
-      status: telemetryData.status,
-      lock: telemetryData.lock, //? "buka" : "tutup"
-      timestamp: telemetryDB[0].timestamp,
+      uid: telemetryDB[0].uid || null,
+      status: telemetryData.status || null,
+      lock: telemetryData.lock || null, //? "buka" : "tutup"
+      timestamp: telemetryDB[0].timestamp || null,
     };
+
     res.status(200).json({
       status: 200,
-
       message: "success",
       data: getData,
     });
--- models/probox.js
+++ models/probox.js
@@ -1,56 +1,32 @@
-const dbConnection = require("../config/dbConfig");
+const supabase = require("../config/supabaseConfig");
 
 const insertHistory = async (uid, status, lock, timestamp) => {
-  const sql = `INSERT INTO history(uid, status, lock, timestamp)
-    VALUES (?, ?, ?, ?)`;
-  const values = [uid, status, lock, timestamp];
+  const { data, error } = await supabase
+    .from("history")
+    .insert([{ uid, status, lock, timestamp }]);
 
-  try {
-    const [result] = await dbConnection.query(sql, values);
-    return result;
-  } catch (error) {
+  if (error) {
     throw error;
   }
+
+  return data;
 };
 
 const getAllHistory = async () => {
-  const query =
-    "SELECT * FROM history WHERE id < (SELECT MAX(id) FROM history) ORDER BY id DESC LIMIT 4";
+  const { data, error } = await supabase
+    .from("history")
+    .select("*")
+    .order("id", { ascending: false })
+    .range(1, 4);
 
-  try {
-    const [results] = await dbConnection.query(query);
-    return results;
-  } catch (error) {
+  if (error) {
     throw error;
   }
-};
 
-const getRowCount = async () => {
-  const query = "SELECT COUNT(*) AS rowCount FROM history";
-
-  try {
-    const [results] = await dbConnection.query(query);
-    const rowCount = results[0].rowCount;
-    return rowCount;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const deleteHistoryData = async (deleteCount) => {
-  const deleteQuery = `DELETE FROM history ORDER BY id ASC LIMIT ${deleteCount}`;
-
-  try {
-    const [result] = await dbConnection.query(deleteQuery);
-    return result.affectedRows; // Mengembalikan jumlah baris yang dihapus
-  } catch (error) {
-    throw error;
-  }
+  return data;
 };
 
 module.exports = {
   insertHistory,
   getAllHistory,
-  getRowCount,
-  deleteHistoryData,
 };
